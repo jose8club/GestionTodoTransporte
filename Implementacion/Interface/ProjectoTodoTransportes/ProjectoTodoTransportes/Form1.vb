329f7c2e93b22218df1a1cc7ad558877431e79ee
Luis Barrientos Fajardo <luzhowbf@gmail.com>
2014-10-09 03:54:03 -0300

    Avances y arreglos en general

--- Implementacion/Interface/ProjectoTodoTransportes/ProjectoTodoTransportes/Form1.vb
+++ Implementacion/Interface/ProjectoTodoTransportes/ProjectoTodoTransportes/Form1.vb
@@ -1,6 +1,5 @@
 ﻿Public Class Form1
-
-
+    Dim con As Conexion = New Conexion
 
     Private Sub DateTimePicker1_ValueChanged(sender As System.Object, e As System.EventArgs) Handles FechAt.ValueChanged
 
@@ -10,23 +9,36 @@
 
     End Sub
 
-    Private Sub ListBox1_SelectedIndexChanged(sender As System.Object, e As System.EventArgs)
+    Private Sub ListBox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs)
 
     End Sub
 
-    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
-        Coneccion.Main()
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+
     End Sub
 
     Private Sub GuardarBtn_Click(sender As System.Object, e As System.EventArgs) Handles GuardarBtn.Click
+        con.RegistrarCliente("asdas", "asd", "asd", "asd")
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        'Crea Array con Cursos y los agrega al combobox
+
+        Dim n As Integer
+        n = con.registrosEnCURSO - 1
+        Dim cursos(n) As String
+        cursos = con.cursosToArray(n)
+        For i As Integer = 0 To n
+            CursoCbx.Items.Add(cursos(i))
+        Next
 
     End Sub
 
-    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
-        Coneccion.Conectar()
+    Private Sub Form1_FormClosing(ByVal sender As System.Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles MyBase.FormClosing
+
     End Sub
 
-    Private Sub Form1_FormClosing(sender As System.Object, e As System.Windows.Forms.FormClosingEventArgs) Handles MyBase.FormClosing
-        Coneccion.Cerrar()
-    End Sub
+
+
 End Class
