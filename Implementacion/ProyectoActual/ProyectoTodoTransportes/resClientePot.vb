8342e7cf414d65caaa7268150d9c93971639911c
jose8club <jose8club@gmail.com>
2014-12-05 00:52:04 -0300

    resumen clientes
    este es el resumen de clientes por funcionarios aun falta por hacer

--- Implementacion/ProyectoActual/ProyectoTodoTransportes/resClientePot.vb
+++ Implementacion/ProyectoActual/ProyectoTodoTransportes/resClientePot.vb
@@ -2,7 +2,7 @@
     Dim con As New Conexion
     Dim USER As String = ""
     Dim STATUS As ToolStripStatusLabel
-
+    Dim items() As String
     Sub New(ByVal usuario As String, ByVal conexion As Conexion, ByVal estado As ToolStripStatusLabel)
         con = conexion
         USER = usuario
@@ -10,6 +10,50 @@
         InitializeComponent()
     End Sub
     Private Sub resClientePot_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
-
+        loadCBOX("Usuario")
     End Sub
+
+#Region "Metodos"
+    Sub loadCBOX(ByVal Nombre As String)
+        Dim n As Integer
+        Dim items2() As String
+        If Nombre.Equals("Usuario") Then
+            cbox_funcionario.Items.Clear()
+
+            n = con.count("Usuario") - 1
+            items2 = con.toArray(n, "Nombre", "Usuario")
+            For i As Integer = 0 To n
+                cbox_funcionario.Items.Add(items2(i))
+            Next
+            If n >= 0 Then cbox_funcionario.SelectedIndex = 0
+            If cbox_funcionario.SelectedIndex Then
+                setItems(cbox_funcionario.Text)
+                cargaLIST("Usuario")
+            End If
+        End If
+    End Sub
+
+    Sub setItems(ByVal Nombre As String)
+        Dim n As Integer
+
+        n = con.countWhere("Atencion_Cliente_Potencial", "Usuario = " & Nombre & "") - 1
+        items = con.toArrayWhere(n, "Cliente_Potencial", "Atencion_Cliente_Potencial", "Usuario = " & Nombre & "")
+    End Sub
+
+    Sub cargaLIST(ByVal Nombre As String)
+        list_clientes.Items.Clear()
+
+        Dim n As Integer
+        If Nombre.Equals("Usuario") Then
+            n = items.Length - 1
+            For i As Integer = 0 To n
+                list_clientes.Items.Add(items(i))
+            Next
+            If list_clientes.Items.Count > 0 Then list_clientes.SelectedIndex = 0
+
+        End If
+    End Sub
+
+#End Region
+
 End Class
