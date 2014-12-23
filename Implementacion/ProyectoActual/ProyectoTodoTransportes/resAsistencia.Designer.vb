8ebe9403a57a913dda1e4de755453b2ef717a42b
jose8club <jose8club@gmail.com>
2014-12-23 18:15:29 -0300

    resumen asistencias

--- Implementacion/ProyectoActual/ProyectoTodoTransportes/resAsistencia.Designer.vb
+++ Implementacion/ProyectoActual/ProyectoTodoTransportes/resAsistencia.Designer.vb
@@ -22,53 +22,107 @@ Partial Class resAsistencia
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim ColumnHeader1 As System.Windows.Forms.ColumnHeader
         Me.cbox_estudiante = New System.Windows.Forms.ComboBox()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.list_asistencia = New System.Windows.Forms.ListBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.list_asistencias = New System.Windows.Forms.ListView()
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.lbl_estudiante = New System.Windows.Forms.Label()
+        ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'cbox_estudiante
         '
-        Me.cbox_estudiante.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbox_estudiante.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest
+        Me.cbox_estudiante.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems
+        Me.cbox_estudiante.DropDownStyle = System.Windows.Forms.ComboBoxStyle.Simple
         Me.cbox_estudiante.FormattingEnabled = True
-        Me.cbox_estudiante.Location = New System.Drawing.Point(141, 51)
+        Me.cbox_estudiante.Location = New System.Drawing.Point(108, 46)
         Me.cbox_estudiante.Name = "cbox_estudiante"
-        Me.cbox_estudiante.Size = New System.Drawing.Size(330, 21)
+        Me.cbox_estudiante.Size = New System.Drawing.Size(225, 21)
         Me.cbox_estudiante.TabIndex = 3
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(24, 54)
+        Me.Label1.Location = New System.Drawing.Point(25, 49)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(60, 13)
         Me.Label1.TabIndex = 2
         Me.Label1.Text = "Estudiante:"
         '
-        'list_asistencia
+        'GroupBox1
         '
-        Me.list_asistencia.FormattingEnabled = True
-        Me.list_asistencia.Location = New System.Drawing.Point(27, 100)
-        Me.list_asistencia.Name = "list_asistencia"
-        Me.list_asistencia.Size = New System.Drawing.Size(444, 173)
-        Me.list_asistencia.TabIndex = 4
+        Me.GroupBox1.Controls.Add(Me.lbl_estudiante)
+        Me.GroupBox1.Controls.Add(Me.list_asistencias)
+        Me.GroupBox1.Controls.Add(Me.cbox_estudiante)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(22, 22)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(577, 284)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Asistencias por Estudiante"
+        '
+        'list_asistencias
+        '
+        Me.list_asistencias.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3})
+        Me.list_asistencias.GridLines = True
+        Me.list_asistencias.LabelEdit = True
+        Me.list_asistencias.Location = New System.Drawing.Point(28, 93)
+        Me.list_asistencias.MultiSelect = False
+        Me.list_asistencias.Name = "list_asistencias"
+        Me.list_asistencias.Size = New System.Drawing.Size(523, 174)
+        Me.list_asistencias.TabIndex = 40
+        Me.list_asistencias.UseCompatibleStateImageBehavior = False
+        Me.list_asistencias.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        ColumnHeader1.Text = "Numero"
+        ColumnHeader1.Width = 1
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Asistencia Teorica"
+        Me.ColumnHeader2.Width = 253
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "Asistencia Teorica"
+        Me.ColumnHeader3.Width = 260
+        '
+        'lbl_estudiante
+        '
+        Me.lbl_estudiante.AutoSize = True
+        Me.lbl_estudiante.Location = New System.Drawing.Point(368, 49)
+        Me.lbl_estudiante.Name = "lbl_estudiante"
+        Me.lbl_estudiante.Size = New System.Drawing.Size(39, 13)
+        Me.lbl_estudiante.TabIndex = 41
+        Me.lbl_estudiante.Text = "Label8"
         '
         'resAsistencia
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(658, 419)
-        Me.Controls.Add(Me.list_asistencia)
-        Me.Controls.Add(Me.cbox_estudiante)
-        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.GroupBox1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "resAsistencia"
         Me.Text = "resAsistencia"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
     Friend WithEvents cbox_estudiante As System.Windows.Forms.ComboBox
     Friend WithEvents Label1 As System.Windows.Forms.Label
-    Friend WithEvents list_asistencia As System.Windows.Forms.ListBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents list_asistencias As System.Windows.Forms.ListView
+    Friend WithEvents ColumnHeader2 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader3 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents lbl_estudiante As System.Windows.Forms.Label
 End Class
--- Implementacion/ProyectoActual/ProyectoTodoTransportes/resAsistencia.vb
+++ Implementacion/ProyectoActual/ProyectoTodoTransportes/resAsistencia.vb
@@ -10,6 +10,57 @@
         InitializeComponent()
     End Sub
     Private Sub resAsistencia_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        loadCBOX("Matricula")
+        lbl_estudiante.Text = con.selectWhereQuery("cl.nombre", "cliente cl, compra co, matricula m", "m.codigocompra = co.idcompra and co.cliente = cl.idcliente and m.codigo ='" & cbox_estudiante.Text & "'")
+    End Sub
+#Region "Métodos"
+    Sub loadCBOX(ByVal Nombre As String)
 
+        Dim n As Integer
+        Dim items() As String
+
+        If Nombre.Equals("Matricula") Then
+            cbox_estudiante.Items.Clear()
+
+            n = con.count("Estudiante") - 1
+            items = con.toArray(n, "idEstudiante", "Estudiante")
+
+            cbox_estudiante.Items.Add("")
+            For i As Integer = 0 To n
+                cbox_estudiante.Items.Add(items(i))
+            Next
+
+            If n >= 0 Then cbox_estudiante.SelectedIndex = 0
+
+        End If
+    End Sub
+
+    Sub loadLista(ByVal fun As String)
+        list_asistencias.Items.Clear()
+        Dim s As Integer
+        Dim n As Integer
+        Dim items() As String
+        Dim items2() As String
+        If fun.Equals("") Then
+            s = 0
+        Else
+            s = CInt(con.selectWhereQuery("min(idATENCION_CLIENTE_POTENCIAL)", "atencion_cliente_potencial", "usuario ='" & fun & "'"))
+        End If
+        n = con.count("Cliente_Potencial") - 1
+        items = con.toArrayWhere(n, "c.Nombre", "atencion_cliente_potencial a, cliente_potencial c", "a.Cliente_Potencial=c.idCliente_Potencial and a.usuario ='" & fun & "' and a.idATENCION_CLIENTE_POTENCIAL = '" & s & "'")
+        items2 = con.toArrayWhere(n, "c.Telefono", "atencion_cliente_potencial a, cliente_potencial c", "a.Cliente_Potencial=c.idCliente_Potencial and a.usuario ='" & fun & "' and a.idATENCION_CLIENTE_POTENCIAL = '" & s & "'")
+        For i As Integer = 0 To n
+            Dim Pago As New ListViewItem("", 0)
+            Pago.SubItems.Add(items(i))
+            Pago.SubItems.Add(items2(i))
+            list_asistencias.Items.AddRange(New ListViewItem() {Pago})
+        Next
+
+    End Sub
+#End Region
+
+    Private Sub cbox_estudiante_SelectedValueChanged(sender As System.Object, e As System.EventArgs) Handles cbox_estudiante.SelectedValueChanged
+        lbl_estudiante.Text = con.selectWhereQuery("cl.nombre", "cliente cl, compra co, matricula m", "m.codigocompra = co.idcompra and co.cliente = cl.idcliente and m.codigo ='" & cbox_estudiante.Text & "'")
+        loadLista(cbox_estudiante.Text)
     End Sub
 End Class
