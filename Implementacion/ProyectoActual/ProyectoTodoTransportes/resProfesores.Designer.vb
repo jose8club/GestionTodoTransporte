09f973673a4fbcaa7f4eb716a19428c20ae01653
jose8club <jose8club@gmail.com>
2014-12-23 16:17:18 -0300

    resumen profesores

--- Implementacion/ProyectoActual/ProyectoTodoTransportes/resProfesores.Designer.vb
+++ Implementacion/ProyectoActual/ProyectoTodoTransportes/resProfesores.Designer.vb
@@ -22,64 +22,107 @@ Partial Class resProfesores
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim ColumnHeader1 As System.Windows.Forms.ColumnHeader
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
-        Me.list_asistencias = New System.Windows.Forms.ListBox()
-        Me.list_aprobados = New System.Windows.Forms.ListBox()
+        Me.cbox_docente = New System.Windows.Forms.ComboBox()
+        Me.list_estudiantesTeo = New System.Windows.Forms.ListView()
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(33, 46)
+        Me.Label1.Location = New System.Drawing.Point(14, 38)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(53, 13)
+        Me.Label1.Size = New System.Drawing.Size(56, 13)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Docentes"
+        Me.Label1.Text = "Docentes:"
         '
-        'ComboBox1
+        'cbox_docente
         '
-        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
-        Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(132, 43)
-        Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(348, 21)
-        Me.ComboBox1.TabIndex = 1
+        Me.cbox_docente.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest
+        Me.cbox_docente.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems
+        Me.cbox_docente.DropDownStyle = System.Windows.Forms.ComboBoxStyle.Simple
+        Me.cbox_docente.FormattingEnabled = True
+        Me.cbox_docente.Location = New System.Drawing.Point(113, 35)
+        Me.cbox_docente.Name = "cbox_docente"
+        Me.cbox_docente.Size = New System.Drawing.Size(427, 21)
+        Me.cbox_docente.TabIndex = 1
         '
-        'list_asistencias
+        'list_estudiantesTeo
         '
-        Me.list_asistencias.FormattingEnabled = True
-        Me.list_asistencias.Location = New System.Drawing.Point(36, 101)
-        Me.list_asistencias.Name = "list_asistencias"
-        Me.list_asistencias.Size = New System.Drawing.Size(308, 186)
-        Me.list_asistencias.TabIndex = 2
+        Me.list_estudiantesTeo.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3})
+        Me.list_estudiantesTeo.GridLines = True
+        Me.list_estudiantesTeo.LabelEdit = True
+        Me.list_estudiantesTeo.Location = New System.Drawing.Point(17, 110)
+        Me.list_estudiantesTeo.MultiSelect = False
+        Me.list_estudiantesTeo.Name = "list_estudiantesTeo"
+        Me.list_estudiantesTeo.Size = New System.Drawing.Size(523, 174)
+        Me.list_estudiantesTeo.TabIndex = 39
+        Me.list_estudiantesTeo.UseCompatibleStateImageBehavior = False
+        Me.list_estudiantesTeo.View = System.Windows.Forms.View.Details
         '
-        'list_aprobados
+        'ColumnHeader1
         '
-        Me.list_aprobados.FormattingEnabled = True
-        Me.list_aprobados.Location = New System.Drawing.Point(379, 101)
-        Me.list_aprobados.Name = "list_aprobados"
-        Me.list_aprobados.Size = New System.Drawing.Size(300, 186)
-        Me.list_aprobados.TabIndex = 3
+        ColumnHeader1.Text = "Numero"
+        ColumnHeader1.Width = 1
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Aprobados"
+        Me.ColumnHeader2.Width = 108
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "Reprobados"
+        Me.ColumnHeader3.Width = 126
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.cbox_docente)
+        Me.GroupBox1.Controls.Add(Me.list_estudiantesTeo)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(36, 25)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(601, 318)
+        Me.GroupBox1.TabIndex = 40
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Profesores"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(14, 83)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(65, 13)
+        Me.Label2.TabIndex = 40
+        Me.Label2.Text = "Estudiantes:"
         '
         'resProfesores
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(720, 461)
-        Me.Controls.Add(Me.list_aprobados)
-        Me.Controls.Add(Me.list_asistencias)
-        Me.Controls.Add(Me.ComboBox1)
-        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.GroupBox1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "resProfesores"
         Me.Text = "resProfesores"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
     Friend WithEvents Label1 As System.Windows.Forms.Label
-    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
-    Friend WithEvents list_asistencias As System.Windows.Forms.ListBox
-    Friend WithEvents list_aprobados As System.Windows.Forms.ListBox
+    Friend WithEvents cbox_docente As System.Windows.Forms.ComboBox
+    Friend WithEvents list_estudiantesTeo As System.Windows.Forms.ListView
+    Friend WithEvents ColumnHeader2 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader3 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
 End Class
--- Implementacion/ProyectoActual/ProyectoTodoTransportes/resProfesores.vb
+++ Implementacion/ProyectoActual/ProyectoTodoTransportes/resProfesores.vb
@@ -10,6 +10,52 @@
         InitializeComponent()
     End Sub
     Private Sub resProfesores_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        loadCBOX("Docentes")
+    End Sub
 
+#Region "Métodos"
+    Sub loadCBOX(ByVal Nombre As String)
+        Dim n As Integer
+        Dim items() As String
+        If Nombre.Equals("Docentes") Then
+            cbox_docente.Items.Clear()
+
+            n = con.count("Docente") - 1
+            items = con.toArrayWhere(n, "a.Nombre", "funcionario a, docente b", "a.idFuncionario=b.idDocente")
+
+            cbox_docente.Items.Add("")
+            For i As Integer = 0 To n
+                cbox_docente.Items.Add(items(i))
+            Next
+
+            If n >= 0 Then cbox_docente.SelectedIndex = 0
+
+        End If
+    End Sub
+
+    Sub loadListaTeo(ByVal docente As String)
+        list_estudiantesTeo.Items.Clear()
+        Dim n As Integer
+        Dim items() As String
+        Dim items2() As String
+        Dim func As Integer = CInt(con.selectWhereQuery("idfuncionario", "funcionario", "nombre ='" & docente & "'"))
+        n = con.count("documento") - 1
+        items = con.toArrayWhere(n, "c.nombre", "cliente c, documento d, estudiante_documento e, matricula m, compra co ", "d.idDocumento=e.Documento and e.estudiante=m.codigo and m.CodigoCompra=co.idCOMPRA and co.cliente=c.idCLIENTE and d.estado='Aprobado' and d.funcionario='" & func & "'")
+        items2 = con.toArrayWhere(n, "c.nombre", "cliente c, documento d, estudiante_documento e, matricula m, compra co ", "d.idDocumento=e.Documento and e.estudiante=m.codigo and m.CodigoCompra=co.idCOMPRA and co.cliente=c.idCLIENTE and d.estado='Reprobado' and d.funcionario='" & func & "'")
+
+        For i As Integer = 0 To n
+            Dim est As New ListViewItem("", 0)
+            est.SubItems.Add(items(i))
+            est.SubItems.Add(items2(i))
+
+            list_estudiantesTeo.Items.AddRange(New ListViewItem() {est})
+        Next
+
+    End Sub
+
+#End Region
+
+    Private Sub cbox_docente_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles cbox_docente.SelectedIndexChanged
+        loadListaTeo(cbox_docente.Text)
     End Sub
 End Class
