1554caa3df16ccad7c86762863a68e2a12d4912e
jose8club <jose8club@gmail.com>
2015-02-17 20:21:03 -0300

    nuevas modificaciones examen
    1) solo los teoricos, practicos y visuales se haran sin aprobacion.
    2) el municipal solo se hara dos veces

--- Implementacion/ProyectoActual/ProyectoTodoTransportes/newExamen.vb
+++ Implementacion/ProyectoActual/ProyectoTodoTransportes/newExamen.vb
@@ -131,8 +131,11 @@
         ElseIf CInt(sbox_Minutos.Text) <> 0 And cbox_TipoExamen.Text.Equals("Cambio Rueda") Then
             MsgBox("La hora debe ser exacta")
             Return False
-        ElseIf aprovado(cbox_RegistroMatricula.SelectedValue.ToString, cbox_TipoExamen.SelectedValue.ToString) Then
+        ElseIf aprovado(cbox_RegistroMatricula.SelectedValue.ToString, cbox_TipoExamen.SelectedValue.ToString) And Not (cbox_TipoExamen.Text.Equals("Examen Municipal") Or cbox_TipoExamen.Text.Equals("Examen Psicotécnico") Or cbox_TipoExamen.Text.Equals("Cambio Rueda")) Then
             MsgBox("Estudiante aprovado en " & cbox_TipoExamen.SelectedValue.ToString & ". No se puede realizar operacion")
+            Return False
+        ElseIf suficiente(cbox_RegistroMatricula.SelectedValue.ToString, cbox_TipoExamen.SelectedValue.ToString) And cbox_TipoExamen.Text.Equals("Examen Municipal") Then
+            MsgBox("Examen Municipal hecho en mas de las ocasiones permitidas. No se puede realizar operacion")
             Return False
         End If
 
@@ -156,6 +159,25 @@
         Else
             Return False
         End If
+    End Function
+
+    Function suficiente(ByVal Matricula As String, ByVal Examen As String) As Boolean
+        Dim oportunidad As Integer = 0
+        Dim d As DataTable = con.doQuery("SELECT count(d.Estado) " _
+                                    & "FROM Documento d, Estudiante_Documento e, Estudiante f" _
+                                     & " WHERE d.idDOCUMENTO=e.Documento and e.Estudiante=f.idESTUDIANTE and f.idESTUDIANTE= '" & Matricula & "' and d.Tipo = '" & Examen & "'")
+
+        If d.Rows.Count > 0 Then
+            oportunidad = CInt(d.Rows(0).Item(0).ToString)
+        Else
+            oportunidad = 0
+        End If
+        If oportunidad > 2 Then
+            Return True
+        Else
+            Return False
+        End If
+        Return True
     End Function
 
     Private Sub cbox_TipoExamen_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cbox_TipoExamen.SelectedIndexChanged
