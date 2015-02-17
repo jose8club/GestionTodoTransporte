893e448b3c4d9bcf78cca8fb02f7d6ea07f4aa3f
Luis Barrientos Fajardo <luzhowbf@gmail.com>
2015-02-16 21:26:48 -0300

    Resumen Curso
    Avances... CASI LISTO

--- Implementacion/ProyectoActual/ProyectoTodoTransportes/ResumenCurso.vb
+++ Implementacion/ProyectoActual/ProyectoTodoTransportes/ResumenCurso.vb
@@ -5,6 +5,7 @@
     Dim dc As DataCBOX
     Dim Data As DataTable
     Dim Query As String
+
     Sub New(ByVal usuario As String, ByVal conexion As Conexion, ByVal estado As ToolStripStatusLabel)
         con = conexion
         USER = usuario
@@ -13,14 +14,12 @@
         InitializeComponent()
     End Sub
 
-    Private Sub tbox_search_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tbox_search.TextChanged
-        componeQuery()
-        Query = Query & " AND Codigo LIKE '%" & tbox_search.Text & "%'"
-        setData()
-
-    End Sub
-
     Private Sub ResumenCurso_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        check_Maquinaria.Checked = True
+        check_Otros.Checked = True
+        check_Particular.Checked = True
+        check_Profesional.Checked = True
+        updateData()
 
     End Sub
 
@@ -29,9 +28,11 @@
             Data = con.doQuery(Query)
             list_Curso.DataSource = Data
             list_Curso.DisplayMember = "Codigo"
+            list_Curso.ValueMember = "Codigo"
         Else
             list_Curso.DataSource = Nothing
         End If
+
     End Sub
 
     Sub componeQuery()
@@ -58,6 +59,21 @@
         Query = Query & ")"
     End Sub
 
+    Sub updateData()
+        Try
+            Dim aux As DataTable = New DataTable
+            aux = con.doQuery("SELECT p.Area FROM Curso c, Producto p WHERE c.Codigo = '" & list_Curso.SelectedValue & "' AND c.Producto = p.Nombre")
+            lbl_Area.Text = "Area:                              " & aux.Rows(0).Item(0).ToString
+            aux = con.doQuery("SELECT p.Nombre FROM Curso c, Producto p WHERE c.Codigo = '" & list_Curso.SelectedValue & "' AND c.Producto = p.Nombre")
+            lbl_Producto.Text = "Producto:                       " & aux.Rows(0).Item(0).ToString
+
+            aux = con.doQuery("SELECT f.Nombre FROM Teoria t, Practica p, Clase c, Funcionario f WHERE c.Curso = '" & list_Curso.SelectedValue & "' AND ((c.idCLASE = t.Clase AND t.Profesor = f.idFuncionario) OR (c.idCLASE = p.Clase AND p.Instructor = f.idFuncionario)) GROUP BY f.Nombre")
+            list_Profesores.DataSource = aux
+            list_Profesores.DisplayMember = "Nombre"
+        Catch ex As Exception
+
+        End Try
+    End Sub
 #Region "CheckedChanged"
 
     Private Sub check_Maquinaria_CheckedChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles check_Maquinaria.CheckedChanged
@@ -82,4 +98,15 @@
 
 #End Region
 
+    Private Sub tbox_search_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tbox_search.TextChanged
+        componeQuery()
+        Query = Query & " AND Codigo LIKE '%" & tbox_search.Text & "%'"
+        setData()
+
+    End Sub
+
+
+    Private Sub list_Curso_SelectedValueChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles list_Curso.SelectedValueChanged
+        updateData()
+    End Sub
 End Class
